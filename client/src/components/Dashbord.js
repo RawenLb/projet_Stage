288b18c9218e7adc3d5b69fd62b16c190b1fc03b
RawenLb <99543648+RawenLb@users.noreply.github.com>
2024-07-24 13:00:33 +0100

    ..

--- client/src/components/Dashbord.js
+++ client/src/components/Dashbord.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import QuestionsList from "./QuestionsList";
 import Admin from "./Admin";
 
 function Dashboard() {
+
+
+  
   return (
     <div className="layout-wrapper layout-content-navbar">
       <div className="layout-container">
         <Sidebar />
         <div className="layout-page">
           <Navbar />
-          <div className="content-wrapper">
-            <Routes>
+          <div className="centered-container">
+          <Routes>
               <Route path="/questions" element={<QuestionsList />} />
               <Route path="/admin" element={<Admin />} />
               {/* Add other routes here */}
